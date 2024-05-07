dc249ce8cb3d4270a6948ad1a22519de60ce0369
Chirag <chirag0264@gmail.com>
2024-05-07 12:13:15 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+// import transactionSimulationRouter from './routes/transactionSimulationRouter';
+// import userRouter from './routes/userRouter';
+// import promptRouter from './routes/promptRouter';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(cookieParser());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false }));
+
+// creating routes
+// app.use("/api", transactionSimulationRouter);
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
+});
